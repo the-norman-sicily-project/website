63936a0fd31e748fd09bbb66b859e8fccb947fcf
Joe Hayes <joephayes@gmail.com>
2021-02-21 22:48:14 -0500

    Adds new deploy script Adds REACT_APP_API_SERVER_HOST and REACT_APP_API_SERVER_PORT to build script

---
+++ scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -ex
+
+AWS_PROFILE=${1:-nsp}
+
+SCRIPTDIR=`dirname "$0"`
+PROJECTDIR=`dirname "$SCRIPTDIR"`
+
+DISTDIR=$PROJECTDIR/dist
+
+BUCKET_NAME=www.normansicily.org
+
+echo checking for required programs
+hash aws 2>/dev/null || { echo >&2 "script requires AWS CLI but it's not installed. Aborting."; exit 1; }
+
+if [[ ! -d "$DISTDIR" ]]; then
+  echo dist directory does not exist - please run build script first!
+  exit 1
+fi
+
+aws s3 sync "$DISTDIR" s3://$BUCKET_NAME --profile $AWS_PROFILE
